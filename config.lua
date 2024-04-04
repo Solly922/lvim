f320075f7b9bf9ce7934e3271fc3fddea4e369fc
Solly922 <solomon.schuster01@gmail.com>
2024-04-04 09:21:40 -0700

    lf to format lol

--- config.lua
+++ config.lua
@@ -19,7 +19,7 @@ lvim.builtin.treesitter.ensure_installed = {
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "goimports", filetypes = { "go" } },
-  { command = "gofumpt", filetypes = { "go" } },
+  { command = "gofumpt",   filetypes = { "go" } },
 }
 
 lvim.format_on_save = {
--- lua/test.go
+++ lua/test.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-println("This is a test")
+	println("This is a test")
 }
