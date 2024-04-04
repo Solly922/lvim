6ac65b9c055aba9a74225b3ee253c1ae39d9e466
Solomon Schuster <solomon.schuster01@gmail.com>
2024-04-04 10:55:53 -0700

    allows for save in insert mode

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,3 +1,4 @@
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.insert_mode["<C-s>"] = "<C-c>:w<cr>a"
